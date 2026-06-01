72ce217489e6e0442be82e4bd360c2af3df13b69
Sskumar05 <sshathiskumar54@gmail.com>
2026-06-01 20:07:38 +0530

    created

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,8 +102,26 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* <head>
+        <HeadContent />
+      </head> */}
       <head>
         <HeadContent />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-914JR0V325"
+        >
+        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-914JR0V325');
+      `,
+          }}
+        />
       </head>
       <body>
         {children}
