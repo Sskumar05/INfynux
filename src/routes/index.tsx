cadcb8ebe23f9c2a83a3f3512aab6c346f6f5bf7
Naveen <naveendseven@gmail.com>
2026-09-01 09:50:59 +0530

    Fix hero section bottom shade in light mode

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -147,7 +147,7 @@ function Hero() {
       </div>
       
       {/* Subtle bottom separator fade */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent pointer-events-none" />
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black to-transparent pointer-events-none" />
     </section>
   );
 }
